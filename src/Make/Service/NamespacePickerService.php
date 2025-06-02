60ab1e7c706dc344740466cf6dc7779e5d0b6775
s.grigarczik <s.grigarczik@agiqon.de>
2025-06-02 17:21:37 +0200

    feat: make admin module command

--- src/Make/Service/NamespacePickerService.php
+++ src/Make/Service/NamespacePickerService.php
@@ -7,10 +7,11 @@ namespace Shopware\Development\Make\Service;
 use RuntimeException;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Path;
-use Symfony\Component\Finder\Finder;
 
 class NamespacePickerService
 {
+    const ADMIN_NAMESPACE = 'Resources/app/administration/src/';
+
     public function pickNamespace(SymfonyStyle $io, array $pluginPath, string $default = ''): array
     {
         $serviceNamespace = $io->ask('Please enter the namespace path starting from src (e.g. Service/MyService)', $default, function ($answer) {
@@ -23,6 +24,21 @@ class NamespacePickerService
 
         $pluginPath['path'] = Path::join($pluginPath['path'], $serviceNamespace);
         $pluginPath['namespace'] = $pluginPath['namespace'] . '\\' . str_replace('/', '\\', $serviceNamespace);
+
+        return $pluginPath;
+    }
+
+    public function pickAdminNamespace(SymfonyStyle $io, array $pluginPath, string $default = ''): array
+    {
+        $serviceNamespace = $io->ask('Please enter the namespace path starting from Resources/app/administration/src (e.g. module/my-module)', $default, function ($answer) {
+            if (empty($answer)) {
+                throw new RuntimeException('The namespace path cannot be empty.');
+            }
+
+            return $answer;
+        });
+
+        $pluginPath['path'] = Path::join($pluginPath['path'], self::ADMIN_NAMESPACE . $serviceNamespace);
 
         return $pluginPath;
     }
