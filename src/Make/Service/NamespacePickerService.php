1d8ffb9feee37cd0328defb128dd0088c019df54
s.grigarczik <s.grigarczik@agiqon.de>
2025-06-02 17:10:43 +0200

    fix: Remove useles methods

--- src/Make/Service/NamespacePickerService.php
+++ src/Make/Service/NamespacePickerService.php
@@ -11,7 +11,7 @@ use Symfony\Component\Finder\Finder;
 
 class NamespacePickerService
 {
-    public function pickNamespace(SymfonyStyle $io, string $pluginPath, string $default = ''): array
+    public function pickNamespace(SymfonyStyle $io, array $pluginPath, string $default = ''): array
     {
         $serviceNamespace = $io->ask('Please enter the namespace path starting from src (e.g. Service/MyService)', $default, function ($answer) {
             if (empty($answer)) {
@@ -21,60 +21,9 @@ class NamespacePickerService
             return $answer;
         });
 
-        $bundleNamespace =  $this->detectNamespaceFromMainClass($pluginPath);
-        if (empty($bundleNamespace)) {
-            $io->error('Could not detect the main plugin class.');
-            throw new RuntimeException('Main plugin class namespace could not be detected.');
-        }
+        $pluginPath['path'] = Path::join($pluginPath['path'], $serviceNamespace);
+        $pluginPath['namespace'] = $pluginPath['namespace'] . '\\' . str_replace('/', '\\', $serviceNamespace);
 
-        $namespace = $bundleNamespace . '\\' . str_replace('/', '\\', $serviceNamespace);
-
-        return [
-            'fullPath' => Path::join($pluginPath, $serviceNamespace),
-            'namespace' => $namespace,
-        ];
-    }
-
-    private function detectNamespaceFromMainClass(string $srcPath): ?string
-    {
-        $finder = new Finder();
-        $finder->files()
-            ->in($srcPath)
-            ->name('*.php')
-            ->depth(0);
-
-        foreach ($finder as $file) {
-            $content = $file->getContents();
-
-            if (preg_match('/^namespace\s+([^;]+);/m', $content, $matches)) {
-                $namespace = trim($matches[1]);
-
-                if ($this->isMainPluginClass($content)) {
-                    return $namespace;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    private function isMainPluginClass(string $content): bool
-    {
-        $patterns = [
-            '/class\s+\w+\s+extends\s+Plugin/',
-            '/class\s+\w+Bundle\s+extends\s+Bundle/',
-            '/class\s+\w+Plugin\s+extends/',
-            '/implements\s+.*PluginInterface/',
-            '/extends\s+.*PluginBootstrap/',
-            '/extends\s+.*BasePlugin/',
-        ];
-
-        foreach ($patterns as $pattern) {
-            if (preg_match($pattern, $content)) {
-                return true;
-            }
-        }
-
-        return false;
+        return $pluginPath;
     }
 }
